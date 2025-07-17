aeda4b73fd68db2243827a4a5b3e84ab380f913b
benbest123 <94733217+benbest123@users.noreply.github.com>
2025-07-18 10:12:53 +1200

    Initial commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "poo poo pee pee",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "wah wah",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "hee hee haa haa",
+    completed: false,
+  },
+];
