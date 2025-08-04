cc0003536ce6d3871cfd747432d0207d41be7e03
benbest123 <94733217+benbest123@users.noreply.github.com>
2025-08-04 15:24:50 +1200

    added logout button

--- frontend/src/pages/todo.tsx
+++ frontend/src/pages/todo.tsx
@@ -1,5 +1,6 @@
 import AddTodoForm from "../components/todo/AddTodoForm";
 import TodoList from "../components/todo/TodoList";
+import TodoNavbar from "../components/todo/TodoNavbar";
 import TodoSummary from "../components/todo/TodoSummary";
 import { useAuth } from "../hooks/useAuth";
 import useTodos from "../hooks/useTodos";
@@ -12,14 +13,17 @@ function Todo() {
     await addTodo(title);
   };
   return (
-    <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">{username}'s todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 round-md p-5 space-y-6">
-        <AddTodoForm onSubmit={handleToDoAdded} />
-        <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo} />
-      </div>
-      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
-    </main>
+    <>
+      <TodoNavbar />
+      <main className="py-10 h-screen space-y-5 overflow-y-auto">
+        <h1 className="font-bold text-3xl text-center">{username}'s todos</h1>
+        <div className="max-w-lg mx-auto bg-slate-100 round-md p-5 space-y-6">
+          <AddTodoForm onSubmit={handleToDoAdded} />
+          <TodoList todos={todos} onCompletedChange={setTodoCompleted} onDelete={deleteTodo} />
+        </div>
+        <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
+      </main>
+    </>
   );
 }
 
