64c9e04eed0353958a5db4789a85144b6e803d73
benbest123 <94733217+benbest123@users.noreply.github.com>
2025-09-09 09:17:27 +1200

    style auth page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -2,10 +2,12 @@ import AuthForm from "../components/auth/AuthForm";
 
 function Auth() {
   return (
-    <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Register or Log In</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 round-md p-5 space-y-6">
-        <AuthForm />
+    <main className="py-10 h-screen space-y-5 overflow-y-auto bg-[#008080]">
+      <div className="max-w-lg mx-auto bg-[#C0C0C0] round-md p-1 space-y-2 shadow-w95Container">
+        <div className="max-w-lg mx-auto bg-[#000080] text-white px-2 py-1">Register or Login</div>
+        <div className="max-w-lg mx-auto shadow-w95InnerContainer p-3">
+          <AuthForm />
+        </div>
       </div>
     </main>
   );
