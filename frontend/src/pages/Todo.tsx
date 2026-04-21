b504ae6149d739ef48b3c04c102f4e7649ed79fb
=ben <benbest987@gmail.com>
2026-04-21 15:41:25 +1200

    [GENAI=NO] fix: removed unnecessary function wrapper

--- frontend/src/pages/Todo.tsx
+++ frontend/src/pages/Todo.tsx
@@ -9,16 +9,13 @@ function Todo() {
   const { todos, addTodo, setTodoCompleted, deleteAllCompletedTodos, deleteTodo, reorderTodos } = useTodos();
   const { username } = useAuth();
 
-  const handleToDoAdded = async (title: string) => {
-    await addTodo(title);
-  };
   return (
     <div className='flex flex-col h-screen'>
       <main className='py-10 flex-1 space-y-5 overflow-y-auto bg-[#008080]'>
         <div className='max-w-lg mx-auto bg-[#C0C0C0] round-md p-1 space-y-2 shadow-w95Container'>
           <div className='max-w-lg mx-auto bg-[#000080] text-white px-2 py-1'>{username}'s todos</div>
           <div className='max-w-lg mx-auto shadow-w95InnerContainer p-3'>
-            <AddTodoForm onSubmit={handleToDoAdded} />
+            <AddTodoForm onSubmit={addTodo} />
           </div>
           <div className='max-w-lg mx-auto shadow-w95InnerContainer p-3'>
             <TodoList
